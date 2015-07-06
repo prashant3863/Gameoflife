3c332758ad2a18195105c90656dacff83554865a
Prashant <prashantee@iitj.ac.in>
2015-07-06 11:41:11 +0530

    Second passing spec

--- lib/application/cell.rb
+++ lib/application/cell.rb
@@ -1,13 +1,18 @@
 class Cell
 	
 	DEAD = "dead"
-	
+	ALIVE = "alive"
+
 	def initialize(state)
 		@state = state
 	end
 
 	def dead?
     @state == DEAD
+	end
+
+	def alive?
+		@state == ALIVE
 	end
 end
 
--- spec/application/cell_spec.rb
+++ spec/application/cell_spec.rb
@@ -2,11 +2,16 @@ require "spec_helper"
 
 describe "cell" do
 
-  it "cell can be dead" do
-  state = "dead"
-  cell = Cell.new(state)
-  expect(cell).to be_dead
+  it "cell is dead" do
+    state = "dead"
+    cell = Cell.new(state)
+    expect(cell).to be_dead
   end
-  
+
+  it "cell is alive" do
+    state = "alive"
+    cell =Cell.new(state)
+    expect(cell).to be_alive
+  end
 end
 
